4f0d31f2e49f039831d181dcd9aff27d9800c621
Michael-Shep <mgShepherd1504@gmail.com>
2021-07-31 21:24:59 +0100

    Added validation code to all inputs in the create new user page

--- chatterbox/src/components/SignUp.js
+++ chatterbox/src/components/SignUp.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import "firebase/auth";
 
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
@@ -9,37 +10,62 @@ import { InputAdornment } from '@material-ui/core';
 
 const SignUp = () => {
     const [email, setEmail] = useState("");
-    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
+    const [validEmail, setValidEmail] = useState(true);
+    const [validPassword, setValidPassword] = useState(true);
+    const [validConfirmPassword, setValidConfirmPassword] = useState(true);
+
+    const emailRegex = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+    const passwordRegex = new RegExp(/^(?=[a-zA-Z_]*\d)(?=[0-9_]*[a-zA-Z])[a-zA-Z0-9_]{8,}$/);
+
+    const performValidationWithRegex = (regex, testStr, isValidFunction) => {
+        if (!regex.test(testStr)) {
+            isValidFunction(false);
+        }
+        else {
+            isValidFunction(true);
+        }
+    };
+
+    useEffect(() => {
+        if (email.length > 0) {
+            performValidationWithRegex(emailRegex, email, setValidEmail);
+        }
+    }, [email]);
+
+    useEffect(() => {
+        if (password.length > 0) {
+            performValidationWithRegex(passwordRegex, password, setValidPassword);
+        }
+    }, [password]);
+
+    useEffect(() => {
+        if (confirmPassword === password || confirmPassword.length === 0) {
+            setValidConfirmPassword(true);
+        } 
+        else {
+            setValidConfirmPassword(false);
+        }
+    }, [confirmPassword]);
+
     const signUpButtonHandler = () => {
-        console.log(`Email: ${email}, Username: ${username}, Password: ${password}, Confirm Password: ${confirmPassword}`);
+        console.log(`Email: ${email}, Password: ${password}, Confirm Password: ${confirmPassword}`);
     };
 
-    const emailValidiationHandler = () => {
-        return true;
+    const shouldSubmitBeDisabled = () => {
+        return !validEmail || email.length === 0 || !validPassword || password.length === 0 || 
+                            !validConfirmPassword || validConfirmPassword.length === 0;
     };
 
-    const usernameValidationHandler = () => {
-        return true;
-    };
-
-    const passwordValidationHandler = () => {
-        return true;
-    };
-
-    const confirmPasswordValidationHandler = () => {
-        return true;
-    };
-
-    const createFormElement = (labelString, value, setterFunction, tooltipString, validationFunction, errorString) => {
-        let validInput = validationFunction();
+    const createFormElement = (labelString, value, setterFunction, tooltipString, validInput, errorString, inputType='text') => {
         let helperText = validInput ? '' : errorString;
         return (
             <Box mb={2}>
                 <TextField fullWidth label={labelString} variant="filled" value={value}
-                           onChange={(e) => setterFunction(e.target.value)} error={!validInput} helperText={helperText}
+                           onChange={(e) => { setterFunction(e.target.value); }} 
+                           error={!validInput} helperText={helperText} type={inputType}
                            InputProps={{endAdornment: (
                                <Tooltip className="inputToolTip" title={tooltipString} placement="top" arrow>
                                    <InputAdornment position="end">
@@ -60,15 +86,14 @@ const SignUp = () => {
                 <Box mt={-5}> <h3>Create New User</h3> </Box>
                 <form className="formPadding">
                     {createFormElement('Email', email, setEmail, 'Must be standard email format e.g. example@example.com',
-                                        emailValidiationHandler, 'Input must be a valid email')}
-                    {createFormElement('Username', username, setUsername, 'Can contain letters, numbers and underscores',
-                                        usernameValidationHandler, 'Must only contain letters, numbers and underscores')}
+                                        validEmail, 'Input must be a valid email')}
                     {createFormElement('Password', password, setPassword, 'At least 8 characters and contain at least 1 letter and digit',
-                                        passwordValidationHandler, 'Must contain at least 8 characters, 1 digit and 1 letter')}
+                                        validPassword, 'At least 8 characters, 1 digit and 1 letter', 'password')}
                     {createFormElement('Confirm Password', confirmPassword, setConfirmPassword, 'Passwords must match',
-                                        confirmPasswordValidationHandler, 'Passwords must match')}
+                                        validConfirmPassword,'Passwords must match', 'password')}
                     <Box mb={3}> 
-                        <Button variant="contained" className="loginButton" onClick={signUpButtonHandler}>
+                        <Button variant="contained" className="loginButton" onClick={signUpButtonHandler}
+                                disabled={shouldSubmitBeDisabled()}>
                             Sign Up
                         </Button> 
                     </Box>
